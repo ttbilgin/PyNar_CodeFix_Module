59799f23a0bf8ed8e9a131bf702f27b4cf1ac9c4
ttbilgin <ttbilgin@gmail.com>
2022-09-14 23:04:34 +0300

    Pynar editör hata düzeltme modülü

---
+++ pynar_patch.bat
@@ -0,0 +1,8 @@
+copy Data\CodeDataset\CodeDataset.txt ..\PyNar_Beta-main\Data\CodeDataset\CodeDataset.txt /Y
+copy Data\CodeDataset\ImportDataset.txt ..\PyNar_Beta-main\Data\CodeDataset\ImportDataset.txt /Y
+
+del ..\PyNar_Beta-main\Data\CodeDataset\CodeDataset.py /Y
+
+copy Components\ChatBotView\chatbotview.py ..\PyNar_Beta-main\Components\ChatBotView\chatbotview.py /Y
+
+copy Components\CheckError\*.py ..\PyNar_Beta-main\Components\CheckError\  /Y
