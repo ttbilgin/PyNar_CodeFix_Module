21f35b3e0cab09fdc33a8e054c6b425347faf7e2
ttbilgin <ttbilgin@gmail.com>
2022-09-14 23:12:32 +0300

    Update pynar_patch.bat

--- pynar_patch.bat
+++ pynar_patch.bat
@@ -1,7 +1,7 @@
 copy Data\CodeDataset\CodeDataset.txt ..\PyNar_Beta-main\Data\CodeDataset\CodeDataset.txt /Y
 copy Data\CodeDataset\ImportDataset.txt ..\PyNar_Beta-main\Data\CodeDataset\ImportDataset.txt /Y
 
-del ..\PyNar_Beta-main\Data\CodeDataset\CodeDataset.py /Y
+del ..\PyNar_Beta-main\Data\CodeDataset\CodeDataset.py
 
 copy Components\ChatBotView\chatbotview.py ..\PyNar_Beta-main\Components\ChatBotView\chatbotview.py /Y
 
